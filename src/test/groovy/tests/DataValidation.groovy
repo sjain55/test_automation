b0a23e9836d32bb1f1af498bb84fbdc8efd97e87
Parmveer Nijher <pnijher@manh.com>
2018-09-07 09:55:22 +0530

    Added DB validation Assertion.

--- src/test/groovy/tests/DataValidation.groovy
+++ src/test/groovy/tests/DataValidation.groovy
@@ -1,9 +1,16 @@
 package tests
-
+/**
+ * Created by Parmveer Singh Nijher and Abhishek Nagpurkar
+ * Created a test method inside the class to test the DB validation framework.
+ * Date: 4 spetember 2018
+ * This class is a test class to perform the DB validation of existing data in DB.
+ * Info: Tihs class is a dummy class to verify the DB validation framework.
+ */
 import com.manh.test.util.commonutil.TestNGWMUtil
 import com.manh.test.util.dataloader.BaseTest
 import com.manh.test.util.logger.TestLogger
 import com.manh.test.util.validation.WMValidationUtil
+import org.testng.Assert
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
@@ -16,24 +23,30 @@ class DataValidation {
     TreeMap<String, String> whereConditionKeyValuePairs
     TreeMap<String, String> expectedKeyValuePairs
     BaseTest loader
+
+    /*Creating the instance of logger, TestNGWMUtil, BaseTest, wmValidationUtil classes. */
+
     @BeforeClass
-    public void preconfig(){
+    public void preconfigure() {
         log = TestLogger.getLogger(this.getClass().getSimpleName());
         twm = new TestNGWMUtil(this.getClass().getSimpleName(), this.log)
+        /*Load the properties file indicated by the class name*/
         loader = twm.getBaseTest()
         wmValidationUtil = twm.wmValidationUtil
     }
+
+    /*Test to verify whether the Shipment is created having same data in DB or not.*/
+
     @Test
-    public void dataValidation()
-    {
+    public void dataValidation() {
         whereConditionKeyValuePairs = new TreeMap<String, String>();
         whereConditionKeyValuePairs.put("SHIPMENT_ID", loader.fetchValue("SHIPMENT_ID"))
-        /*expectedKeyValuePairs = new TreeMap<String, String>()
+        expectedKeyValuePairs = new TreeMap<String, String>()
         expectedKeyValuePairs.put("ORG_ID", loader.fetchValue("ORG_ID"))
         expectedKeyValuePairs.put("TRANSPORTATION_STATUS", loader.fetchValue("TRANSPORTATION_STATUS"))
         expectedKeyValuePairs.put("TENDER_STATUS", loader.fetchValue("TENDER_STATUS"))
-        expectedKeyValuePairs.put("TOTAL_COST", loader.fetchValue("TOTAL_COST"))*/
-        wmValidationUtil.validateFor("SHIPMENT_TLM_FLOW",whereConditionKeyValuePairs)
+        expectedKeyValuePairs.put("TOTAL_COST", loader.fetchValue("TOTAL_COST"))
+        Assert.assertEquals(wmValidationUtil.validateFor("SHIPMENT_TLM_FLOW", whereConditionKeyValuePairs, expectedKeyValuePairs), true)
 
     }
 
